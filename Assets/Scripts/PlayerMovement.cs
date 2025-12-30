be29222352099476532c8cad7f35ee65c99b7581
unknown <arigator.t@gmail.com>
2025-12-30 22:05:54 +0000

    Added left and right movements

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,21 +3,29 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 1f;
+
     private Vector2 moveInput;
+    Rigidbody2D myRigidbody2D;
 
     private void Start()
     {
-        
+        myRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        
+        Run();
     }
 
     private void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+    }
+
+    private void Run()
+    {
+        Vector2 playerVelocity = new Vector2 (moveInput.x * moveSpeed, myRigidbody2D.linearVelocityY);
+        myRigidbody2D.linearVelocity = playerVelocity;
     }
 }
