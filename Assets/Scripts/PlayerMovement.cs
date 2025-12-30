3bf671e4dc2491cb0a908084cf8cec7e98982ebf
unknown <arigator.t@gmail.com>
2025-12-30 22:53:20 +0000

    polished running animation and added jumping mechanic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,27 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float jumpSpeed = 1f;
 
     private Vector2 moveInput;
-    Rigidbody2D myRigidbody2D;
+
+    private Rigidbody2D myRigidbody2D;
+    private Animator myAnimator;
 
     private void Start()
     {
         myRigidbody2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     private void Update()
     {
         Run();
+        FlipSprite();
     }
 
     private void OnMove(InputValue value)
@@ -23,9 +29,37 @@ public class PlayerMovement : MonoBehaviour
         moveInput = value.Get<Vector2>();
     }
 
+    private void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            myRigidbody2D.linearVelocity += new Vector2 (0f, jumpSpeed);
+        }
+    }
+
     private void Run()
     {
         Vector2 playerVelocity = new Vector2 (moveInput.x * moveSpeed, myRigidbody2D.linearVelocityY);
+        bool hasHorizontalSpeed = Mathf.Abs(myRigidbody2D.linearVelocityX) > Mathf.Epsilon;
+
         myRigidbody2D.linearVelocity = playerVelocity;
+        if (hasHorizontalSpeed)
+        {
+            myAnimator.SetBool("isRunning", true);
+        }
+        else
+        {
+            myAnimator.SetBool("isRunning", false);
+        }
+    }
+
+    private void FlipSprite()
+    {
+        bool hasHorizontalSpeed = Mathf.Abs(myRigidbody2D.linearVelocityX) > Mathf.Epsilon;
+
+        if (hasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2 (Mathf.Sign(myRigidbody2D.linearVelocityX), 1f);
+        }
     }
 }
