d67b5f26211962eca26813f697bbe6bd8fcd75ea
unknown <arigator.t@gmail.com>
2025-12-31 16:51:49 +0000

    Added ladder climbing and animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,23 +6,27 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 1f;
     [SerializeField] private float jumpSpeed = 1f;
+    [SerializeField] private float climbSpeed = 1f;
 
     private Vector2 moveInput;
 
     private Rigidbody2D myRigidbody2D;
     private Animator myAnimator;
     private CapsuleCollider2D myCapsuleCollider2D;
+    private float gravityStart;
 
     private void Start()
     {
         myRigidbody2D = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         myCapsuleCollider2D = GetComponent<CapsuleCollider2D>();
+        gravityStart = myRigidbody2D.gravityScale;
     }
 
     private void Update()
     {
         Run();
+        Climb();
         FlipSprite();
     }
 
@@ -54,6 +58,30 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             myAnimator.SetBool("isRunning", false);
+        }
+    }
+
+    private void Climb()
+    {   
+        Vector2 playerVelocity = new Vector2 (myRigidbody2D.linearVelocityX , moveInput.y * climbSpeed);
+        bool hasVerticalSpeed = Mathf.Abs(myRigidbody2D.linearVelocityY) > Mathf.Epsilon;
+        int climbingLayer = LayerMask.GetMask("Climbing");
+
+        if (!myCapsuleCollider2D.IsTouchingLayers(climbingLayer))
+        {
+            myRigidbody2D.gravityScale = gravityStart;
+            return;
+        }
+        
+        myRigidbody2D.linearVelocity = playerVelocity;
+        myRigidbody2D.gravityScale = 0;
+        if (hasVerticalSpeed)
+        {
+            myAnimator.SetBool("isClimbing", true);
+        }
+        else
+        {
+            myAnimator.SetBool("isClimbing", false);
         }
     }
 
