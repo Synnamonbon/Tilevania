5b82d2d46774666f0555b4cba733117c0d57e801
unknown <arigator.t@gmail.com>
2026-01-01 15:13:35 +0000

    Added SFX to coins

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] private AudioClip coinPickupSFX;
+
+    private bool wasCollected = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && !wasCollected)
         {
+            wasCollected = true;
+            AudioSource.PlayClipAtPoint(coinPickupSFX, transform.position);
             Destroy(gameObject);
         }
     }
