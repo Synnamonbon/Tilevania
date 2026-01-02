ca84ca7bee45266cc6dd21893678a5d260ac3cf3
unknown <arigator.t@gmail.com>
2026-01-02 16:30:41 +0000

    Added level progression by coroutines

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -1,11 +1,30 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelExit : MonoBehaviour
 {
+    [SerializeField] private float levelLoadDelay = 1f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex + 1);
+        StartCoroutine(LoadNextLevel());
     }
+
+    private IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+
+    }
+
 }
